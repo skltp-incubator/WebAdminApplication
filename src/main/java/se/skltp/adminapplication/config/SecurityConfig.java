8efc75129f35d58b4a53b2fc25cea6dce4629143
Hans Bragman <hans.bragman@soprasteria.com>
2020-12-16 10:02:19 +0100

    Updated java-files and reverted to older spring-boot in pom.

--- src/main/java/se/skltp/adminapplication/config/SecurityConfig.java
+++ src/main/java/se/skltp/adminapplication/config/SecurityConfig.java
@@ -1,23 +1,40 @@
+/**
+ * Copyright (c) 2014 Center for eHalsa i samverkan (CeHis).
+ * 								<http://cehis.se/>
+ *
+ * This file is part of SKLTP.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package se.skltp.adminapplication.config;
 
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Profile("default")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
+  /*@Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth)
       throws Exception {
     auth.inMemoryAuthentication().withUser("user")
@@ -29,59 +46,58 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated()
         .and().formLogin()
         .loginPage("/login").permitAll();
-  }
+  }*/
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
-  /*@Autowired
+  @Autowired
+  private DataSource dataSource;
+
+  @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth)
       throws Exception {
-    *//* With this embedded auth in place there is no need to use a datasource to lookup the users *//*
-    auth.inMemoryAuthentication()
-        .withUser("user").password("password").roles("USER");
-  }*/
+    auth.jdbcAuthentication().dataSource(dataSource)
+        .passwordEncoder(new ShaPasswordEncoder())
+        .usersByUsernameQuery("select anvandarnamn AS username, losenord_hash AS password, true AS enabled from Anvandare where anvandarnamn = ?")
+        .authoritiesByUsernameQuery("select anvandarnamn AS username, CASE WHEN administrator = 1 THEN 'ROLE_ADMIN' ELSE 'ROLE_USER' END AS authority from Anvandare where anvandarnamn = ?");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/**").hasAnyRole("ADMIN", "USER")
+        .anyRequest().anonymous().and().httpBasic();
+  }
 
   /*@Autowired
   private DataSource dataSource;
 
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) {
-    //throws Exception {
-    try {
-      Connection con = dataSource.getConnection("tpadminuser", "tpadminpassword");
-      String query = "select anvandarnamn AS username, losenord_hash AS password, true AS enabled from Anvandare where anvandarnamn = 'hbragman'";
-      try (Statement stmt = con.createStatement()) {
-        ResultSet rs = stmt.executeQuery(query);
-        while (rs.next()) {
-          String anvnamn = rs.getString("username");
-          String password = rs.getString("password");
-          System.out.println(anvnamn);
-        }
-      } catch (SQLException e) {
-        System.out.println(e.getMessage());
-      }
+  @Bean
+  public PasswordEncoder delegatingPasswordEncoder() {
+    PasswordEncoder defaultEncoder = new StandardPasswordEncoder();
+    Map<String, PasswordEncoder> encoders = new HashMap<>();
+    encoders.put("sha256", new StandardPasswordEncoder());
+    encoders.put("scrypt", new SCryptPasswordEncoder());
 
-      auth.jdbcAuthentication().dataSource(dataSource)
-          .passwordEncoder(new ShaPasswordEncoder())
-          .usersByUsernameQuery("select anvandarnamn AS username, losenord_hash AS password, true AS enabled from Anvandare where anvandarnamn = ?")
-          .authoritiesByUsernameQuery("select anvandarnamn AS username, CASE WHEN administrator = 1 THEN 'ROLE_ADMIN' ELSE 'ROLE_USER' END AS authority from Anvandare where anvandarnamn = ?");
-    }catch(Exception e) {
-      System.out.println(e.getMessage());
-    }
+    DelegatingPasswordEncoder passworEncoder = new DelegatingPasswordEncoder(
+        "sha256", encoders);
+    passworEncoder.setDefaultPasswordEncoderForMatches(defaultEncoder);
+
+    return passworEncoder;
   }
 
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth)
+      throws Exception {
+    auth.jdbcAuthentication().dataSource(dataSource)
+        .passwordEncoder(delegatingPasswordEncoder())
+        .usersByUsernameQuery("select anvandarnamn AS username, losenord_hash AS password, true AS enabled from Anvandare where anvandarnamn = ?")
+        .authoritiesByUsernameQuery("select anvandarnamn AS username, CASE WHEN administrator = 1 THEN 'ROLE_ADMIN' ELSE 'ROLE_USER' END AS authority from Anvandare where anvandarnamn = ?");
+  }
 
   @Override
-  protected void configure(HttpSecurity http) { //throws Exception {
-    try {
-      http.authorizeRequests()
-          .antMatchers("/**").hasAnyRole("ADMIN", "USER")
-          .anyRequest().anonymous().and().httpBasic();
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-    }
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/**").hasAnyRole("ADMIN", "USER")
+        .anyRequest().anonymous().and().httpBasic();
   }*/
 }
 
